d0b8d86006d8a298c8f3622ea573c686dccefae1
Damien Ramelet <damien.ramelet@protonmail.com>
2022-02-13 20:18:23 +0100

    [catchthemall] Start project
    Create the project. Create pokedex app, suitable models and migrations.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# pokedex
+Catch Them All !
+
+Django/DRF technical test
+
+[ER diagram](https://drive.google.com/file/d/1pwEx8WcwaotmdP_JcI5bnyQhjb0eyfvj/view?usp=sharing)
---
+++ pokedex/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class PokemonType(models.Model):
+    pokemon_type = models.CharField(max_length=16) 
+
+
+class PokedexCreature(models.Model):
+    name = models.CharField(max_length=32)
+    primary_type = models.ForeignKey(PokemonType, on_delete=models.PROTECT, related_name='primary_type')
+    secoundary_type = models.ForeignKey(PokemonType, on_delete=models.PROTECT, blank=True, related_name='secoundary_type')
+    total = models.IntegerField()
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+    sp_atk = models.IntegerField()
+    sp_def = models.IntegerField()
+    speed = models.IntegerField()
+    generation = models.IntegerField()
+    legendary = models.BooleanField(default=False)
+
+
+
