4914e67fdb82286c8ce3dd5d6102d2e924e42bea
Damien Ramelet <damien.ramelet@protonmail.com>
2022-02-13 22:09:19 +0100

    [pokedex/models] Update PokedexCreature models
    Setup UniqueConstraint about pokemon_type and pokemon name

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.db.models import UniqueConstraint
 
 
 class PokemonType(models.Model):
     pokemon_type = models.CharField(max_length=16) 
 
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['pokemon_type'],
+                name='unique_pokemon_type'
+            )
+        ]
 
 class PokedexCreature(models.Model):
     pokemon_id = models.IntegerField()
-    name = models.CharField(max_length=32, primary_key=True)
+    name = models.CharField(max_length=32)
     primary_type = models.ForeignKey(PokemonType, on_delete=models.PROTECT, related_name='primary_type')
     secoundary_type = models.ForeignKey(PokemonType, on_delete=models.PROTECT, blank=True, related_name='secoundary_type')
     total = models.IntegerField()
@@ -19,3 +27,11 @@ class PokedexCreature(models.Model):
     speed = models.IntegerField()
     generation = models.IntegerField()
     legendary = models.BooleanField(default=False)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=['name'],
+                name='unique_pokemon'
+            )
+        ]
