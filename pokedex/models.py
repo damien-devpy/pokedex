7f833d2b71bf865e5313bdd1f8ae123bdeb3281c
Damien Ramelet <damien.ramelet@protonmail.com>
2022-02-14 07:56:39 +0100

    [pokedex/management] Create fill_pokedex command and suitable tests

--- pokedex/models.py
+++ pokedex/models.py
@@ -15,9 +15,9 @@ class PokemonType(models.Model):
 
 class PokedexCreature(models.Model):
     pokemon_id = models.IntegerField()
-    name = models.CharField(max_length=32)
-    primary_type = models.ForeignKey(PokemonType, on_delete=models.PROTECT, related_name='primary_type')
-    secoundary_type = models.ForeignKey(PokemonType, on_delete=models.PROTECT, blank=True, related_name='secoundary_type')
+    name = models.CharField(max_length=32, blank=False)
+    _primary_type = models.ForeignKey(PokemonType, on_delete=models.PROTECT, related_name='primary_type')
+    _secoundary_type = models.ForeignKey(PokemonType, on_delete=models.PROTECT, null=True, related_name='secoundary_type')
     total = models.IntegerField()
     health_point = models.IntegerField()
     attack = models.IntegerField()
@@ -27,6 +27,30 @@ class PokedexCreature(models.Model):
     speed = models.IntegerField()
     generation = models.IntegerField()
     legendary = models.BooleanField(default=False)
+
+    @classmethod
+    def get_fields(cls):
+        return [
+            field.name 
+            for field in cls._meta.get_fields()
+            if field.name != 'id'
+        ]
+
+    @classmethod
+    def get_mandatory_fields(cls):
+        return [
+            field.name
+            for field in cls._meta.get_fields()
+            if field.name != 'id' and not field.null 
+        ]
+
+    @property
+    def primary_type(self):
+        return self._primary_type.pokemon_type
+
+    @property
+    def secoundary_type(self):
+        return self._secoundary_type.pokemon_type
 
     class Meta:
         constraints = [
