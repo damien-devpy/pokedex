53203fb91f094839b31a542eca004eff149114b2
Damien Ramelet <damien.ramelet@protonmail.com>
2022-02-15 20:54:31 +0100

    [pokedex] Proper aliasing of PokedexCreature.*_type
    We don't expose private attribute in our API

--- pokedex/serializers.py
+++ pokedex/serializers.py
@@ -1,25 +1,35 @@
 from rest_framework import serializers
 
-from .models import PokedexCreature
+from .models import PokedexCreature, PokemonType
+
+
+class PokemonTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PokemonType
+        fields = "__all__"
 
 
 class PokedexCreatureSerializer(serializers.ModelSerializer):
     class Meta:
         model = PokedexCreature
         fields = [
+            "id",
+            "pokemon_id",
             "name",
             "total",
             "health_point",
             "legendary",
             "generation",
-            "_secoundary_type",
-            "_primary_type",
+            "primary_type",
+            "secoundary_type",
         ]
-        depth = 1
 
 
 class PokedexCreatureDetailSerializer(serializers.ModelSerializer):
+    primary_type = PokemonTypeSerializer(source="_primary_type")
+    secoundary_type = PokemonTypeSerializer(source="_secoundary_type")
+
     class Meta:
         model = PokedexCreature
-        fields = "__all__"
+        exclude = ["_primary_type", "_secoundary_type"]
         depth = 1
